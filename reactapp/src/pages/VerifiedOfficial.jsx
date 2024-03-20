695b37420d5919f777a4a50eb47b7ccd86b90758
anurag <anurag23095@gmail.com>
2024-03-20 06:33:15 +0530

    Added support for marking proposals

--- reactapp/src/pages/VerifiedOfficial.jsx
+++ reactapp/src/pages/VerifiedOfficial.jsx
@@ -28,22 +28,110 @@ const UserList = ({ verificationHandler, accounts }) => {
     )
 }
 
-const SingleProposal = () => {
-    return(
-        <h1>Single proposal</h1>
-    )
-}
-
-const Proposal = () => {
+const SingleProposal = ({ index }) => {
     return(
         <>
+            <h1>Single proposal of index {index}</h1>
         </>
     )
 }
 
-const ProposalList = () => {
+const Proposal = ({ handleClick, contract, index}) => {
+    const [title, setTitle] = useState('Loading...');
+    const [desc, setDesc] = useState('Loading...');
+    const [phone, setPhone] = useState('Loading...');
+    const [votes, setVotes] = useState('Loading...');
+    const [resolved, setResolved] = useState(false);
+    const dispatch = useNotification();
+
+    const fetchProposal = async () => {
+        const prop = await contract.getProposal(index);
+        let dataUrl = prop['3']
+        let resp = await fetch('https://ipfs.io/ipfs/'+dataUrl)
+        let data = await resp.json();
+        setTitle(data['title'])
+        setDesc(data['description'])
+        setPhone(data['phone'])
+        setVotes(String(prop['1']));
+        setResolved(prop['2']);
+    }
+    useEffect(() => {
+        fetchProposal()
+    }, [])
+
+    const markResolved = async () => {
+        try{
+            await contract.markProposalAsResolved(index)
+            dispatch({
+                type: 'success',
+                message: 'Prposal Resolved',
+                title: 'Proposal marked as resolved',
+                position: 'topR'
+            })
+        } catch (err) {
+            dispatch({
+                type: 'error',
+                message: String(err),
+                title: 'Something went wrong',
+                position: 'topR'
+            })
+        }
+    }
+
     return(
-        <h1>Proposal list</h1>
+        <Box mx='100' onClick={handleClick}>
+            <Flex mx='100' border='1px solid black' p='20' align={'center'} backgroundColor={resolved ? '#AAFF00' : '#FF474D'} m='20' justify={'space-evenly'}>
+                <Text fontSize="20">
+                    {title}
+                </Text>
+                <Spacer />
+                <Text fontSize='20'>
+                    {votes}
+                </Text>
+                { !resolved ? <Button m='5' p='5' mx='30' fontSize={'20'} backgroundColor={'#051C2C'} color={"white"} onClick={markResolved}>Mark Resolved</Button> : null}
+            </Flex>
+        </Box>
+    )
+}
+
+const ProposalList = ({ handleClick }) => {
+    const [contract, setContract] = useState([]);
+
+    const allProducts = gql`
+    {
+        proposalCreateds{
+            resident
+            index
+        }
+    }
+    `;
+
+    const populateContracts = async () => {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const temp = new ethers.Contract('dai.tokens.ethers.eth', PROPOSAL_ABI, signer);
+        const contract = temp.attach(PROPOSAL_ADDRESS)
+        setContract(contract)
+    }
+
+    useEffect(() => {
+        populateContracts()
+    }, [])
+
+    const { loading, data, error } = useQuery(allProducts);
+
+    const fetchProps = () => {
+        let proposals = data['proposalCreateds'];
+        return (<Box>
+                {proposals.map(prop => <Proposal handleClick={handleClick} key={prop.index} contract={contract} index={prop.index}/>)}
+            </Box>
+        )
+    }
+
+    return(
+        <>
+            {loading ? <PuffLoader /> : fetchProps()}
+        </>
     )
 }
 
@@ -116,13 +204,17 @@ const VerifiedOfficial = ({ address }) => {
         return <UserList verificationHandler={verifyUser} accounts={accounts}/>
     }
 
+    const handleClick = (idx) => {
+        setSingleProposal(idx)
+    }
+
     return (
         <Box>
-            <Box>
-                <Button onClick={() => {setShowUsers(true);setShowProposals(false);setSingleProposal(null)}}>Show Users</Button>
-                <Button onClick={() => {setShowUsers(false); setShowProposals(true);setSingleProposal(null)}}>Show Proposals</Button>
-            </Box>
-            {loading ? <Center><PuffLoader my='50' size={200} /></Center> : singleProposal ? <SingleProposal /> : showUsers ?  checkUnverifiedUsers() : <ProposalList />}
+            <Center m='10' p='10'>
+                <Button backgroundColor={'#051C2C'} fontSize='20' color={'white'} m='20' p='10' onClick={() => {setShowUsers(true);setShowProposals(false);setSingleProposal(null)}}>Show Users</Button>
+                <Button backgroundColor={'#051C2C'}  fontSize='20' color={'white'} m='20' p='10' onClick={() => {setShowUsers(false); setShowProposals(true);setSingleProposal(null)}}>Show Proposals</Button>
+            </Center>
+            {loading ? <Center><PuffLoader my='50' size={200} /></Center> : singleProposal ? <SingleProposal index={idx} /> : showUsers ?  checkUnverifiedUsers() : <ProposalList handleClick={handleClick} />}
         </Box>
     )
 }
